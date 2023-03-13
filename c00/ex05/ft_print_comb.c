72f3d8398877fed6c656b558bba982a4ac8429a5
Lucas Fernando Lopes Fischer <llopes-f@c1r2s5.42porto.com>
2023-03-13 20:10:09 +0000

    update exercicios

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -9,25 +9,25 @@
 /*   Updated: 2023/03/12 21:15:42 by llopes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include<unistd.h>
 
-void	aux_print (num1, num2, num3)
+void	aux_print(char num1, char num2, char num3)
 {
-	 if(num1 != '7' || num2 !='8' || num3 != '9')
-	 {
-		write(1, &num1, 1);
-		write(1, &num2, 1);
-		write(1, &num3, 1);
+	write(1, &num1, 1);
+	write(1, &num2, 1);
+	write(1, &num3, 1);
+	if (num1 != '7' || num2 != '8' || num3 != '9')
+	{
 		write(1, ",", 1);
+		write(1, " ", 1);
 	}
 }
 
 void	ft_print_comb(void)
 {
-	int	num1;
-	int	num2;
-	int	num3;
+	char	num1;
+	char	num2;
+	char	num3;
 
 	num1 = '0';
 	num2 = '0';
@@ -43,13 +43,8 @@ void	ft_print_comb(void)
 				aux_print (num1, num2, num3);
 				num3++;
 			}
-		num2++;
+			num2++;
 		}
-	num1++;
+		num1++;
 	}
-}
-
-int main (void)
-{
-	ft_print_comb();
 }
--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -9,50 +9,55 @@
 /*   Updated: 2023/03/10 04:05:41 by llopes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 
-void ft_print_comb2(void)
+void	ft_putchar(char c)
 {
-    char num1;
-    char num2;
-    char num3;
-    char num4;
+	write(1, &c, 1);
+}
 
-    num1 = '0';
-    num2 = '0';
-    num3 = '0';
-    num4 = '1';
+void	print_two_digit_number(int number)
+{
+	if (number < 10)
+	{
+		ft_putchar('0');
+		ft_putchar(number + '0');
+	}
+	else
+	{
+		ft_putchar(number / 10 + '0');
+		ft_putchar(number % 10 + '0');
+	}
+}
 
-    while (num1 <= '9')
-    {
-        while (num2 <= '9')
-        {
-            while (num3 <= '9')
-            {
-                while (num4 <= '9')
-                {
-                    if (num1 < num3 || (num1 == num3 && num2 < num4))
-                    {
-                        write(1, &num1, 1);
-                        write(1, &num2, 1);
-                        write(1, " ", 1);
-                        write(1, &num3, 1);
-                        write(1, &num4, 1);
-                        if (!(num1 == '9' && num2 == '8' && num3 == '9' && num4 == '9'))
-                        {
-                            write(1, ", ", 2);
-                        }
-                    }
-                    num4++;
-                }
-                num4 = '0';
-                num3++;
-            }
-            num3 = '0';
-            num2++;
-        }
-        num2 = '0';
-        num1++;
-    }
+void	print_two_numbers(int first, int second)
+{
+	print_two_digit_number(first);
+	ft_putchar(' ');
+	print_two_digit_number(second);
+	if (first != 98 || second != 99)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	first;
+	int	second;
+
+	first = 0;
+	second = 0;
+	while (first <= 99)
+	{
+		second = first + 1;
+		while (second <= 99)
+		{
+			print_two_numbers(first, second);
+			second++;
+		}
+		first++;
+	}
+}
 }
