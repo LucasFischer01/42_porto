8a787e210f82d958d7f66a47cf8ca6af29fe5edf
Lucas Fernando Lopes Fischer <llopes-f@c1r4s3.42porto.com>
2023-04-13 22:04:23 +0100

    Primeiro commit cursus & alteracao de diretorios

---
+++ 42_porto_cursus/Libft/functions/ft_atoi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llopes-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 20:12:31 by llopes-f          #+#    #+#             */
+/*   Updated: 2023/04/13 20:12:33 by llopes-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<stdio.h>
+
+int     ft_atoi(const char *str)
+{
+        int     sign;
+        int     result;
+
+        result = 0;
+        sign = 1;
+        while (*str == ' ' || (*str >= 7 && *str <= 13))
+                str++;
+        if (*str == '-' || *str == '+')
+        {
+                if(*str == '-')
+                        sign *= -1;
+                str++;
+        }
+        while (*str >= '0' && *str <= '9')
+        {
+                result = result * 10 + ( *str - '0');
+                str++;
+        }
+        return (result * sign);
+}
+#include<stdlib.h>
+
+int     main()
+{
+        printf("atoi = %d \n", atoi("  +347656745465"));
+        printf("ft_atoi = %d", ft_atoi("  +347656745465"));
+
+}
