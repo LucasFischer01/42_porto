5f03fc5e0f68791973388f46ae6b4188c913441c
Lucas Fernando Lopes Fischer <llopes-f@c1r12s13.42porto.com>
2023-06-26 20:04:22 +0100

    update

--- 42_porto_cursus/Get_Next_line/gnl3/get_next_line.h
+++ 42_porto_cursus/Get_Next_line/gnl3/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: llopes-f <llopes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 20:22:32 by llopes-f          #+#    #+#             */
-/*   Updated: 2023/06/18 03:45:20 by llopes-f         ###   ########.fr       */
+/*   Updated: 2023/06/26 20:03:08 by llopes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,14 @@
 # include <string.h>
 # include <fcntl.h>
 
-//			Structs				//
-typedef	struct	s_line
-{
-	char		*buffer;
-	char		*line;
-	size_t		i;
-	int			fd;
-}		t_line;	
-
 //			Buffer				//
 # ifndef BUFFER_SIZE
-#	define BUFFER_SIZE 42
-#endif
+#  define BUFFER_SIZE 42
+# endif
 
 //			Functions			//
-
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
-char	*ft_strjoin (char *s1, char *s2);
+char	*ft_strjoin(char *s1, char *s2);
+
 #endif
--- 42_porto_cursus/Get_Next_line/gnl3/get_next_line_utils.c
+++ 42_porto_cursus/Get_Next_line/gnl3/get_next_line_utils.c
@@ -6,47 +6,47 @@
 /*   By: llopes-f <llopes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 20:23:01 by llopes-f          #+#    #+#             */
-/*   Updated: 2023/06/18 03:45:41 by llopes-f         ###   ########.fr       */
+/*   Updated: 2023/06/26 20:03:10 by llopes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include"get_next_line.h"
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(const char *str )
 {
-	size_t i;
+	int	i;
 
 	i = 0;
 	if (!str)
 		return (0);
-	while (str && str[i] != '\n')
+	while (str[i] && str[i] != '\n')
 		i++;
 	if (str[i] == '\n')
 		i++;
 	return (i);
 }
 
-char	*ft_strjoin (char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	int	i;
-	int	j;
-	char	*str;
+	int		i;
+	int		v;
+	char	*newstr;
 
 	i = 0;
-	j = 0;
-	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
+	v = 0;
+	newstr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!newstr)
 		return (NULL);
-	while (s1 && s1[i])
-		str[j++] = s1[i++];
-	i = 0;
-	while (s2 && s2[i])
+	while (s1 && s1[v])
+		newstr[i++] = s1[v++];
+	v = 0;
+	while (s2 && s2[v])
 	{
-		str[j++] = s2[i++];
-		if (s2[i] == '\n')
+		newstr[i] = s2[v++];
+		if (newstr[i++] == '\n')
 			break ;
 	}
-	str[j] = '\0';
-	free(s1);
-	return (str);
+	newstr[i] = '\0';
+	free (s1);
+	return (newstr);
 }
