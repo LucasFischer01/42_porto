35fa8df305ff7686c2104b74e27390879ef4673f
Lucas Fischer <fischer-lucas@hotmail.com>
2023-09-20 22:43:48 +0100

    update

---
+++ 42_porto_cursus/Push-Swap/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fischer <fischer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 19:53:30 by fischer           #+#    #+#             */
+/*   Updated: 2023/09/20 21:30:56 by fischer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+//	Librairies	//
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//	Structures	//
+
+#endif
