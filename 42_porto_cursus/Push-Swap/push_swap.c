da247157a8d6fd260a2b156b08c7bb37717a8737
Lucas Fischer <fischer-lucas@hotmail.com>
2023-10-02 20:29:34 +0100

    update

--- 42_porto_cursus/Push-Swap/push_swap.c
+++ 42_porto_cursus/Push-Swap/push_swap.c
@@ -3,30 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-f <llopes-f@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fischer <fischer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 20:24:44 by fischer           #+#    #+#             */
-/*   Updated: 2023/09/29 19:20:41 by llopes-f         ###   ########.fr       */
+/*   Updated: 2023/10/02 20:28:46 by fischer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int ft_stacknew(int data, t_stack *stack_a)
+{
 
+        if (!(stack_a = (t_stack *)malloc(sizeof(t_stack))))
+                return (0);
+        stack_a->value = data;
+        stack_a->next = NULL;
+        return (1);
+}
 
 int main(int argc, char *argv[])
 {
-	int cont = 1;
-	t_stack	*stack_a;
-	t_stack	*stack_b;
+        int cont = 1;
+        t_stack *stack_a;
+        t_stack *stack_b;
 
-	
-	
-	if (argc < 2)
-		return (0);
-	while (cont < argc)
-	{
-		printf("%s\n", argv[cont]);
-		cont++;
-	}
+        stack_a = NULL;
+        stack_b = NULL;
+
+        if (argc < 2)
+                return (0);
+        while (cont < argc)
+        {
+                stack_a = ft_stacknew(ft_atoi(argv[cont]), stack_a);
+                printf("%s\n", argv[cont]);
+                cont++;
+        }
+        ft_stackprint(stack_a);
+        ft_stackprint(stack_b);
+        return (0);
 }
--- 42_porto_cursus/Push-Swap/utils.c
+++ 42_porto_cursus/Push-Swap/utils.c
@@ -3,35 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-f <llopes-f@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fischer <fischer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 19:22:03 by llopes-f          #+#    #+#             */
-/*   Updated: 2023/09/29 19:22:16 by llopes-f         ###   ########.fr       */
+/*   Updated: 2023/10/02 20:01:52 by fischer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
-int	ft_atoi(char *str)
+int ft_atoi(char *str)
 {
-	int	i;
-	int	sign;
-	int	result;
+        int i;
+        int sign;
+        int result;
 
-	result = 0;
-	i = 0;
-	sign = 1;
-	while (str[i] == ' ' || (str[i] >= 7 && str[i] <= 13))
-		i++;
-	while (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	return (sign * result);
+        result = 0;
+        i = 0;
+        sign = 1;
+
+        if (str[i] == '-' || str[i] == '+')
+        {
+                if (str[i] == '-')
+                        sign *= -1;
+                i++;
+        }
+        while (str[i] >= '0' && str[i] <= '9')
+        {
+                result = result * 10 + (str[i] - '0');
+                i++;
+        }
+        return (sign * result);
+}
+
+int main(int ac, char *av[])
+{
+        int cont;
+
+        cont = 0;
+
+        if (ac < 2)
+                return (printf("error"));
+        while (++cont < ac)
+        {
+                printf("%d\n", ft_atoi(av[cont]));
+        }
 }
