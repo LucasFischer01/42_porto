04b7584e1499bc8c0e665506375a4bf055b7d916
Lucas Fernando Lopes Fischer <llopes-f@c1r14s13.42porto.com>
2023-10-03 17:12:24 +0100

    update

--- 42_porto_cursus/Push-Swap/push_swap.c
+++ 42_porto_cursus/Push-Swap/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fischer <fischer@student.42.fr>            +#+  +:+       +#+        */
+/*   By: llopes-f <llopes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 20:24:44 by fischer           #+#    #+#             */
-/*   Updated: 2023/10/02 20:28:46 by fischer          ###   ########.fr       */
+/*   Updated: 2023/10/03 16:18:08 by llopes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,9 @@ int main(int argc, char *argv[])
         while (cont < argc)
         {
                 stack_a = ft_stacknew(ft_atoi(argv[cont]), stack_a);
+                stack_a->index = 0;
                 printf("%s\n", argv[cont]);
                 cont++;
         }
-        ft_stackprint(stack_a);
-        ft_stackprint(stack_b);
         return (0);
 }
