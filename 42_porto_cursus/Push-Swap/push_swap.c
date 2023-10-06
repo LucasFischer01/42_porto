9409e30c0fc84dd1c91d7bf040b78e0dbb81d745
Lucas Fernando Lopes Fischer <llopes-f@c1r15s11.42porto.com>
2023-10-06 14:41:22 +0100

    update

--- 42_porto_cursus/Push-Swap/push_swap.c
+++ 42_porto_cursus/Push-Swap/push_swap.c
@@ -6,39 +6,61 @@
 /*   By: llopes-f <llopes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 20:24:44 by fischer           #+#    #+#             */
-/*   Updated: 2023/10/03 16:18:08 by llopes-f         ###   ########.fr       */
+/*   Updated: 2023/10/06 14:40:50 by llopes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int ft_stacknew(int data, t_stack *stack_a)
+void	print_list(t_stack **head)
 {
+	t_stack *cur;
+	
+	cur = *head;
 
-        if (!(stack_a = (t_stack *)malloc(sizeof(t_stack))))
-                return (0);
-        stack_a->value = data;
-        stack_a->next = NULL;
-        return (1);
+	while(cur)
+	{
+		printf("%d, %d", cur->value, cur->index);
+		cur = cur->next;
+	}
+}
+
+t_stack *add_newnode(int data, t_stack *stack)
+{
+	t_stack	*new;
+
+    if (!(new = (t_stack *)malloc(sizeof(t_stack))))
+            return (NULL);
+    new->value = data;
+	new->index = 0;
+	new->head = true;
+    new->next = NULL;
+    new->prev = NULL;
+    return (new);
 }
 
 int main(int argc, char *argv[])
 {
-        int cont = 1;
-        t_stack *stack_a;
-        t_stack *stack_b;
+    int cont = 1;
+    t_stack *stack_a;
+	t_stack *stack_b;
+	int	index;
 
-        stack_a = NULL;
-        stack_b = NULL;
-
-        if (argc < 2)
-                return (0);
-        while (cont < argc)
-        {
-                stack_a = ft_stacknew(ft_atoi(argv[cont]), stack_a);
-                stack_a->index = 0;
-                printf("%s\n", argv[cont]);
-                cont++;
-        }
-        return (0);
+	stack_a = NULL;
+	stack_b = NULL;
+	index = 0;
+	if (argc < 2)
+		return (0);
+	while (cont < argc)
+	{
+		if (index == 0)
+			ft_head(ft_atoi(argv[cont]), stack_a);
+		else
+		{
+			ft_lstadd_back(&stack_a, ft_lstnew(ft_atoi(argv[cont])) );
+			cont++;
+		}
+		index++;
+	}
+	return (0);
 }
--- 42_porto_cursus/Push-Swap/push_swap.h
+++ 42_porto_cursus/Push-Swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: llopes-f <llopes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 19:53:30 by fischer           #+#    #+#             */
-/*   Updated: 2023/09/29 17:02:57 by llopes-f         ###   ########.fr       */
+/*   Updated: 2023/10/06 13:07:49 by llopes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ typedef	struct s_stack
 {
 	int	value;
 	unsigned int	index;
+	bool				head;
 	struct s_stack	*next;
 	struct s_stack	*prev;
 }	t_stack;
